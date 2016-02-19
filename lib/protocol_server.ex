2e230ea63fd0b88a0ddf4dda56ddd44b6a1e480c
Akash Manohar J <akash@akash.im>
2016-02-19 23:03:43 +0530

    Rename

--- lib/protocol_server.ex
+++ lib/protocol_server.ex
@@ -1,8 +1,8 @@
 defmodule HnProtocol.ProtocolServer do
   use GenServer
   @behaviour :ranch_protocol
-
-  @timeout Application.get_env :hn_protocol, :timeout, 100000
+  @timeout 100000
+  @api_base_url "https://hacker-news.firebaseio.com/v0"
 
 
   def start_link(ref, socket, transport, options) do
@@ -30,8 +30,14 @@ defmodule HnProtocol.ProtocolServer do
     output = data
     |> String.split
     |> cmd
-    :ok = transport.send socket, "#{output}\r\n"
-    {:noreply, state, @timeout}
+
+    case output do
+      :quit ->
+        {:stop, :normal, state}
+      value ->
+        :ok = transport.send socket, "#{value}\r\n"
+        {:noreply, state, @timeout}
+    end
   end
 
   def handle_info({:tcp_closed, _socket}, state) do
@@ -47,28 +53,35 @@ defmodule HnProtocol.ProtocolServer do
   end
 
 
-  def cmd(["top"]) do
-    {:ok, req} = HTTPoison.get("https://hacker-news.firebaseio.com/v0/topstories.json")
+  def cmd(["QUIT"]) do
+    :quit
+  end
+
+
+  def cmd(["TOP"]) do
+    {:ok, req} = HTTPoison.get("#{@api_base_url}/topstories.json")
     story_ids = Poison.decode!(req.body)
     |> Enum.take(10)
     |> Enum.join("\r\n")
   end
 
-  def cmd(["ask"]) do
-    {:ok, req} = HTTPoison.get("https://hacker-news.firebaseio.com/v0/askstories.json")
+
+  def cmd(["ASK"]) do
+    {:ok, req} = HTTPoison.get("#{@api_base_url}/askstories.json")
     story_ids = Poison.decode!(req.body)
     |> Enum.take(10)
     |> Enum.join("\r\n")
   end
 
 
-  def cmd(["show", story_id]) do
-    {:ok, req} = HTTPoison.get("https://hacker-news.firebaseio.com/v0/item/#{story_id}.json")
+  def cmd(["SHOW", story_id]) do
+    {:ok, req} = HTTPoison.get("#{@api_base_url}/item/#{story_id}.json")
     story = Poison.decode!(req.body)
     "#{story["title"]}\r\n#{story["by"]}\r\n#{story["text"] || story["url"]}"
   end
 
+
   def cmd(_) do
-    "Not a valid command"
+    "INVALID"
   end
 end
